fd0dd07ecd4731d74454d42540d5ceb485a0b4aa
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-07-13 00:19:10 +0200

    Add .env support
    Install dotenv module, ignore .env file and add .env.example file

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import & configure dotenv package
+require('dotenv').config()
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
