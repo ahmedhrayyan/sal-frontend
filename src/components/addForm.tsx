20b1a5e2898b36a7dfb01c1336dfec4d16d895b5
Your Name <Your mail>
2021-08-24 09:03:13 +0200

    remove unused imports

--- src/components/addForm.tsx
+++ src/components/addForm.tsx
@@ -1,29 +1,29 @@
-import { FunctionComponent, FormEvent, FocusEventHandler } from "react";
+import { FunctionComponent, FormEvent } from "react";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
 
 import {
-	Stack,
-	Button,
-	Flex,
-	Textarea,
-	Modal,
-	ModalOverlay,
-	ModalBody,
-	ModalContent,
-	ModalCloseButton,
-	ModalHeader,
-	ModalFooter,
-	Spacer,
-	useBreakpointValue,
-	Icon,
-	Input,
-	HStack,
-	Text,
+  Stack,
+  Button,
+  Flex,
+  Textarea,
+  Modal,
+  ModalOverlay,
+  ModalBody,
+  ModalContent,
+  ModalCloseButton,
+  ModalHeader,
+  ModalFooter,
+  Spacer,
+  useBreakpointValue,
+  Icon,
+  Input,
+  HStack,
+  Text,
 } from "@chakra-ui/react";
-import AutoTextArea from "./textArea";
 import { BsCardImage } from "react-icons/bs";
 import UserAvatar from "./userAvatar";
 import { AnswerNotes, QuestionNotes } from "./notes";
+import AutoTextArea from "./textArea";
 
 interface AddFormProps {
   user: any; // change later
@@ -38,22 +38,22 @@ interface AddFormProps {
 }
 
 type Inputs = {
-	textarea: string;
-	image: any;
+  textarea: string;
+  image: any;
 };
 
 const AddForm: FunctionComponent<AddFormProps> = ({
-	user,
-	isQuestion,
-	hasImageFeature,
-	isOpen,
-	textareaValue,
-	onChangeHandler,
-	onAddHandler,
-	onClose,
-	onCancelHandler,
+  user,
+  isQuestion,
+  hasImageFeature,
+  isOpen,
+  textareaValue,
+  onChangeHandler,
+  onAddHandler,
+  onClose,
+  onCancelHandler,
 }) => {
-	const respSize = useBreakpointValue({ base: "xs", md: "sm" });
+  const respSize = useBreakpointValue({ base: "xs", md: "sm" });
   const {
     control,
     register,
@@ -65,103 +65,105 @@ const AddForm: FunctionComponent<AddFormProps> = ({
     onAddHandler(data);
     console.log(data);
   };
-	
-	return (
-		<Modal
-			isOpen={isOpen}
-			onClose={onClose}
-			isCentered
-			size="xl"
-			scrollBehavior="inside"
-			useInert={false} //hide all except modal for screen readers
-		>
-			<ModalOverlay />
-			<ModalContent>
-			<ModalHeader>
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+      size="xl"
+      scrollBehavior="inside"
+      useInert={false} //hide all except modal for screen readers
+      
+    >
+      <ModalOverlay />
+      <ModalContent>
+        
+        <ModalHeader>
           <UserAvatar
             name={user.full_name}
             title={user.job}
             imgSrc={user.avatar}
           />
-				</ModalHeader>
-				<ModalCloseButton color="blue.500" fontSize={respSize} />
-				<ModalBody>
-					<Stack as="form" onSubmit={handleSubmit(onSubmitHandler)}>
-						<Controller
-							name="textarea"
-							control={control}
-							render={({ field: { onChange } }) => (
-								<Textarea
-									placeholder={
-										isQuestion
-											? `What's your question, Hossam?`
-											: "Write Answer...."
-									}
-									as={AutoTextArea}
-									value={textareaValue}
-									onChange={(e) => {
-										onChangeHandler(e);
-										onChange(textareaValue);
-									}}
-									variant="flushed"
-									overflow="hidden"
-									resize="none"
-									minHeight="0"
-									maxHeight="100vh"
-									autoFocus
-									fontSize={["xs", "md"]}
-								/>
-							)}
-						/>
+        </ModalHeader>
+        <ModalCloseButton color="blue.500" fontSize={respSize}/>
+        <ModalBody>
+          <Stack as="form" onSubmit={handleSubmit(onSubmitHandler)}>
+            <Controller
+              name="textarea"
+              control={control}
+              render={({ field: { onChange } }) => (
+                <Textarea
+                  placeholder={
+                    isQuestion
+                      ? `What's your question, ${user.first_name}?`
+                      : "Write Answer...."
+                  }
+                  as={AutoTextArea}
+                  value={textareaValue}
+                  onChange={(e) => {
+                    onChangeHandler(e);
+                    onChange(textareaValue);
+                  }}
+                  variant="flushed"
+                  overflow="hidden"
+                  resize="none"
+                  minHeight="0"
+                  maxHeight="100vh"
+                  autoFocus
+                  fontSize={["xs", "md"]}
+                />
+              )}
+            />
 
-						<Flex w="100%">
-							{hasImageFeature && (
-								<HStack
-									color="blue.500"
-									spacing="1"
-									as="label"
-									htmlFor="img-upload"
-									_hover={{ cursor: "pointer" }}
-								>
-									<Icon as={BsCardImage} boxSize={[4, 6]} pb="0" />
-									<Text color="inherit" fontSize={respSize} variant="link">
-										Add Image
-									</Text>
-									<Input
-										id="img-upload"
-										d="none"
-										type="file"
-										accept="image/png, image/jpeg"
-										{...register("image")}
-									/>
-								</HStack>
-							)}
-							<Spacer />
-							<Button
+            <Flex w="100%">
+              {hasImageFeature && (
+                <HStack
+                  color="blue.500"
+                  spacing="1"
+                  as="label"
+                  htmlFor="img-upload"
+                  _hover={{ cursor: "pointer" }}
+                >
+                  <Icon as={BsCardImage} boxSize={[4, 6]} pb="0" />
+                  <Text color="inherit" fontSize={respSize} variant="link">
+                    Add Image
+                  </Text>
+                  <Input
+                    id="img-upload"
+                    d="none"
+                    type="file"
+                    accept="image/png, image/jpeg"
+                    {...register("image")}
+                  />
+                </HStack>
+              )}
+              <Spacer />
+              <Button
                 size={respSize}
                 variant="link"
                 mr={[2, 4, 6]}
                 onClick={onCancelHandler}
                 boxShadow="none" // disable global box shadow for form buttons
               >
-								Cancel
-							</Button>
-							<Button
-								type="submit"
-								size={respSize}
-								boxShadow="none" // disable global box shadow for form buttons
-							>
-								Add {isQuestion ? "Qustion" : "Answer"}
-							</Button>
-						</Flex>
-					</Stack>
-				</ModalBody>
-				<ModalFooter justifyContent={isQuestion ? "center" : "start"} pt="4">
-					{isQuestion ? <QuestionNotes /> : <AnswerNotes />}
-				</ModalFooter>
-			</ModalContent>
-		</Modal>
-	);
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                size={respSize}
+                boxShadow="none" // disable global box shadow for form buttons
+              >
+                Add {isQuestion ? "Qustion" : "Answer"}
+              </Button>
+            </Flex>
+          </Stack>
+        </ModalBody>
+        <ModalFooter justifyContent={isQuestion ? "center" : "start"} pt="4">
+          {isQuestion ? <QuestionNotes /> : <AnswerNotes />}
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 };
 
 export default AddForm;
