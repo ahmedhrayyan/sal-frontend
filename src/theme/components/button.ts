8ff427779c1de58d69ad1d478a897a14e4ea8c32
Hossam Okasha <enghossamokashaa@gmail.com>
2021-08-24 05:32:19 +0200

    restyle button component

---
+++ src/theme/components/button.ts
@@ -0,0 +1,22 @@
+const Button = {
+  baseStyle: {
+    rounded: "full",
+  },
+  defaultProps: {
+    colorScheme: "blue",
+  },
+  variants: {
+    black: {
+      color: "white",
+      bg: "black",
+      "&: hover": { bg: "rgba(0, 0, 0, .95)" },
+    },
+    white: {
+      color: "blue.500",
+      bg: "white",
+      "&: hover": { bg: "gray.50" },
+    },
+  },
+};
+
+export default Button;
