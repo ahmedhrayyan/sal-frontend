874000abafff94fba0946382eebd3347855e1523
Your Name <Your mail>
2021-08-24 03:34:06 +0200

    add .storybook initial files

---
+++ .storybook/preview.js
@@ -0,0 +1,25 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { withPerformance } from "storybook-addon-performance"
+import theme from "../src/theme";
+
+export const parameters = {
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
+
+const withChakra = (StoryFn) => {
+	return (
+		<ChakraProvider theme={theme}>
+			<div id="story-wrapper">
+				<StoryFn />
+			</div>
+		</ChakraProvider>
+	);
+};
+
+export const decorators = [withChakra, withPerformance];
