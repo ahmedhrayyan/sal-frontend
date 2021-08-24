88e9317d6505f8943a103762c5aae2d34195b09b
Hossam Okasha <enghossamokashaa@gmail.com>
2021-08-24 03:49:35 +0200

    move cart to the end of text

--- src/components/textArea.tsx
+++ src/components/textArea.tsx
@@ -3,6 +3,7 @@ import {
 	useEffect,
 	useRef,
 	TextareaHTMLAttributes,
+  FocusEventHandler
 } from "react";
 
 const AutoTextArea: FunctionComponent = (
@@ -19,7 +20,16 @@ const AutoTextArea: FunctionComponent = (
 		}
 	}, [props.value]);
 
-	return <textarea {...props} ref={textareaRef} />;
+  // move focus after the last char
+  // ref: https://gist.github.com/piyonishi/409ecbd07f7b86b7da205ad61210a275
+  const moveCaretToEnd: FocusEventHandler<HTMLTextAreaElement> = (e) => {
+    let temp_value = e.target.value;
+    e.target.value = "";
+    e.target.value = temp_value;
+  };
+
+
+	return <textarea {...props} ref={textareaRef} onFocus={moveCaretToEnd}/>;
 };
 
 export default AutoTextArea;
