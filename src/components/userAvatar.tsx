31833b2720f878b5dfb735933b6e9116f4fab6a3
Your Name <Your mail>
2021-08-24 09:38:29 +0200

    enhance userAvatart styles

--- src/components/userAvatar.tsx
+++ src/components/userAvatar.tsx
@@ -1,40 +1,24 @@
 import { FunctionComponent } from "react";
-import {
-  Text,
-  Stack,
-  Avatar,
-  HStack,
-} from "@chakra-ui/react";
+import { Text, Stack, Avatar, HStack } from "@chakra-ui/react";
 
 interface UserAvatarProps {
   imgSrc?: string;
   name: string;
-  title?: string;
-  hasTitle?: boolean;
+  title: string;
 }
 
 const UserAvatar: FunctionComponent<UserAvatarProps> = ({
-  hasTitle,
   name,
-  title=" ",
-  imgSrc=""
+  title = " ",
+  imgSrc = "",
 }) => {
   return (
-    <HStack spacing="2" >
-      <Avatar
-        src={imgSrc}
-        alt="User Avatar"
-        name={name}
-        boxSize="10"
-      />
-      <Stack spacing={-1} fontSize={["xs", "sm"]} >
-        <Text fontWeight={600} >
-          {name}
-        </Text>
-        <Text color="gray.500" >
-          {hasTitle
-            ? title
-            : (`What's your question, ${name.split(" ")[0]}?`)}
+    <HStack spacing="2">
+      <Avatar src={imgSrc} alt="User Avatar" name={name} boxSize="10" />
+      <Stack spacing={-1} fontSize={["xs", "sm"]}>
+        <Text fontWeight={600}>{name}</Text>
+        <Text color="gray.500" isTruncated maxWidth={["60vw", "35vw"]} >
+          {title}
         </Text>
       </Stack>
     </HStack>
