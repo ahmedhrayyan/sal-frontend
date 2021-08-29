774c2a66b57bc3eac9f7fae5ca4a91d611ce6c9a
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-08-29 11:24:20 +0200

    Add BrowserRouter to the App

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from './theme';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme";
 
 // to remove focus for non-keyboard interactions
 // ref: https://chakra-ui.com/docs/migration#css-reset
 import "focus-visible/dist/focus-visible";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // Import & configure dotenv package
-require('dotenv').config()
+require("dotenv").config();
 
 ReactDOM.render(
-  <React.StrictMode>
-		<ChakraProvider theme={theme}>
-			<App />
-		</ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router>
+			<ChakraProvider theme={theme}>
+				<App />
+			</ChakraProvider>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
