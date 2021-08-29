893a75f5da5cbe7f3deec5a52cb78b809a281b61
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-08-29 11:23:52 +0200

    Add withRouter global decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { withPerformance } from "storybook-addon-performance"
+import { withPerformance } from "storybook-addon-performance";
 import theme from "../src/theme";
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { MemoryRouter } from "react-router";
 // to remove focus for non-keyboard interactions
 // ref: https://chakra-ui.com/docs/migration#css-reset
 import "focus-visible/dist/focus-visible";
 
 export const parameters = {
-  viewport: {
-    viewports: INITIAL_VIEWPORTS,
-  },
-  // remove storybook <body></body> padding
-  layout: "fullscreen",
+	viewport: {
+		viewports: INITIAL_VIEWPORTS,
+	},
+	// remove storybook <body></body> padding
+	layout: "fullscreen",
 	actions: { argTypesRegex: "^on[A-Z].*" },
 	controls: {
 		matchers: {
@@ -31,4 +32,12 @@ const withChakra = (StoryFn) => {
 	);
 };
 
-export const decorators = [withChakra, withPerformance];
+// To be able to use react router elements in stories
+// ref: https://github.com/storybookjs/storybook/issues/769#issuecomment-294082103
+const withRouter = (StoryFn) => (
+	<MemoryRouter>
+		<StoryFn />
+	</MemoryRouter>
+);
+
+export const decorators = [withChakra, withRouter, withPerformance];
