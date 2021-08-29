329b41c04933e10f53be926259c7be123822efad
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-08-29 11:04:41 +0200

    Delete unused files

--- src/components/Button.tsx
+++
@@ -1,8 +0,0 @@
-import { Button as ChakraButton, ButtonProps } from "@chakra-ui/react";
-import { FC } from "react";
-
-const Button: FC<ButtonProps> = (props) => {
-	return <ChakraButton {...props}></ChakraButton>
-}
-
-export default Button;
