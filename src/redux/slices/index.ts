310012900cd0d3bade18710fb63cb867fe74b8c4
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-09-08 17:00:19 +0200

    Add usersSlice

--- src/redux/slices/index.ts
+++ src/redux/slices/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import questions from "./questionsSlice";
 import profile from "./profileSlice";
+import users from "./usersSlice";
 
 const rootReducer = combineReducers({
 	questions,
-	profile
+	profile,
+	users,
 });
 
 export default rootReducer;
