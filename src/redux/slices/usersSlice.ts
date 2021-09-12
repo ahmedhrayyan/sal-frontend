cb0ec31cd37bbcb2b41b5cb357350251bd5048f4
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-09-12 23:48:59 +0200

    Handle loading user questions in redux

--- src/redux/slices/usersSlice.ts
+++ src/redux/slices/usersSlice.ts
@@ -4,7 +4,11 @@ import {
 	isFulfilled,
 } from "@reduxjs/toolkit";
 import { handleShowProfile, handleUpdateProfile } from "./profileSlice";
-import { handleLoadQuestions, handleShowQuestion } from "./questionsSlice";
+import {
+	handleLoadQuestions,
+	handleLoadUserQuestions,
+	handleShowQuestion,
+} from "./questionsSlice";
 
 const usersAdapter = createEntityAdapter<User>({
 	selectId: (user) => user.username, // the unique field in users is username
@@ -17,17 +21,27 @@ const slice = createSlice({
 	}),
 	reducers: {},
 	extraReducers: (builder) => {
-		builder.addMatcher(
-			isFulfilled(
-				handleLoadQuestions,
-				handleShowQuestion,
-				handleShowProfile,
-				handleUpdateProfile
-			),
-			(state, { payload }) => {
-				usersAdapter.upsertMany(state, payload.entities.users);
-			}
-		);
+		builder
+			.addCase(handleLoadUserQuestions.fulfilled, (state, { meta }) => {
+				// add fetched questions pages to each user, useful for pagination & conditional fetching
+				const { username, page } = meta.arg;
+				const user = state.entities[username];
+				if (user) {
+					if (!user.fetchedQPages) user.fetchedQPages = [page];
+					else user.fetchedQPages.push(page);
+				}
+			})
+			.addMatcher(
+				isFulfilled(
+					handleLoadQuestions,
+					handleShowQuestion,
+					handleShowProfile,
+					handleUpdateProfile
+				),
+				(state, { payload }) => {
+					usersAdapter.upsertMany(state, payload.entities.users);
+				}
+			);
 	},
 });
 
