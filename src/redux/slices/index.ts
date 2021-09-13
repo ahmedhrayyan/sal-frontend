6811cb47dff87ee6950d5c9557829c6158a80ee7
Hossam Okasha <enghossamokashaa@gmail.com>
2021-09-13 02:52:33 +0200

    Add answer reducer to the root reducer

--- src/redux/slices/index.ts
+++ src/redux/slices/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import questions from "./questionsSlice";
 import profile from "./profileSlice";
 import users from "./usersSlice";
+import answers from "./answerSlice";
 
 const rootReducer = combineReducers({
 	questions,
+	answers,
 	profile,
 	users,
 });
