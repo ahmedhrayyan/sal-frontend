0f29be3fd0647f195e60ac701f3fb19a11c22dd1
Hossam Okasha <enghossamokashaa@gmail.com>
2021-11-05 01:18:19 +0200

    Handle Search page in the Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,27 +15,99 @@ import QuestionView from "../components/questionView";
 import { selectProfile } from "../redux/slices/profileSlice";
 import {
 	handleLoadQuestions,
+	handleSearchQuestions,
 	selectNextQPage,
 	selectQuestions,
+	selectNextQSearchedPage,
+	clearSearch,
 } from "../redux/slices/questionsSlice";
 import { useAppDispatch, useShallowEqSelector } from "../utils/hooks";
+import { useHistory, useLocation } from "react-router";
 
-// add props after redux
-const Home: FC = () => {
+interface HomeProps {
+	isSearchPage?: boolean;
+}
+const Home: FC<HomeProps> = ({ isSearchPage }) => {
 	const dispatch = useAppDispatch();
 	const profile = useShallowEqSelector(selectProfile);
 	const questions = useShallowEqSelector(selectQuestions);
 	const nextQPage = useShallowEqSelector(selectNextQPage);
+	const nextQSearchedPage = useShallowEqSelector(selectNextQSearchedPage);
 	const respSize = useBreakpointValue({ base: "sm", md: "md" });
+
+	const { search } = useLocation(); //cuurent URL
+	const params = new URLSearchParams(search);
+	const history = useHistory();
+
+	// Home Page
 	useEffect(() => {
-		if (nextQPage === 1) dispatch(handleLoadQuestions(1));
-		document.title = "Sal";
+		if (!isSearchPage) {
+			dispatch(handleLoadQuestions(1));
+			document.title = "Sal - Home";
+		}
 	}, []); //eslint-disable-line
+
+	// Search Page
+	useEffect(() => {
+		if (isSearchPage) {
+			dispatch(clearSearch());
+			if (params.get("searchTerm")) {
+				const searchTerm = params.get("searchTerm") as string;
+				dispatch(
+					handleSearchQuestions({
+						searchTerm,
+						page: 1,
+					})
+				);
+				document.title = `${searchTerm} - Search Results | Sal`;
+			}
+		}
+	}, [search]); //eslint-disable-line
+
+	useEffect(() => {
+		if (isSearchPage) {
+			history.listen((location) => {
+				if (history.action === "POP") {
+					window.location.href = window.location.origin;
+				}
+			});
+		}
+	});
+
+	const handleLoadMore = () => {
+		if (isSearchPage && params.get("searchTerm")) {
+			// Search Page
+			dispatch(
+				handleSearchQuestions({
+					searchTerm: params.get("searchTerm") as string,
+					page: nextQSearchedPage,
+				})
+			);
+		} else {
+			// Home Page
+			dispatch(handleLoadQuestions(nextQPage));
+		}
+	};
 
 	return (
 		<Center mt="13vh">
 			<VStack maxW="xl" w="full" spacing="6">
-				<QuestionForm user={profile} />
+				{isSearchPage ? (
+					// Search Page
+					<Button
+						alignSelf="start"
+						onClick={() => {
+							window.location.href = window.location.origin;
+						}}
+						w={[20, 32]}
+						h={[7, 10]}
+					>
+						Back
+					</Button>
+				) : (
+					//Home Page
+					<QuestionForm user={profile} />
+				)}
 				{questions.ids.map((id) => (
 					<QuestionView
 						key={id}
@@ -52,12 +124,7 @@ const Home: FC = () => {
 					))}
 				{questions.ids.length === questions.total || (
 					<>
-						<Button
-							size={respSize}
-							onClick={() => {
-								dispatch(handleLoadQuestions(nextQPage));
-							}}
-						>
+						<Button size={respSize} onClick={handleLoadMore}>
 							Load More
 						</Button>
 						<Box />
