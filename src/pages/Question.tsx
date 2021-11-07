803626cb44d53afae0c4b09282c5c709d36cded8
Hossam Okasha <enghossamokashaa@gmail.com>
2021-11-07 21:19:34 +0200

    Refetch the Q in case it hadn't

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -1,35 +1,60 @@
-import { Button, Stack, Center } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Stack,
+	Center,
+	SkeletonCircle,
+	SkeletonText,
+} from "@chakra-ui/react";
 import { useParams, useHistory } from "react-router-dom";
 import QuestionView from "../components/questionView";
-import { FC } from "react";
-import { useShallowEqSelector } from "../utils/hooks";
+import { FC, useEffect } from "react";
+import { useShallowEqSelector, useAppDispatch } from "../utils/hooks";
 import { selectProfile } from "../redux/slices/profileSlice";
-import { selectQuestion } from "../redux/slices/questionsSlice";
+import {
+	handleShowQuestion,
+	selectQuestion,
+	selectQStatus,
+} from "../redux/slices/questionsSlice";
 
 interface QuestionViewProps {}
 const Question: FC<QuestionViewProps> = () => {
 	const history = useHistory();
 	const { qId } = useParams() as { qId: string | undefined };
 	const currentUser = useShallowEqSelector(selectProfile);
-	const question = useShallowEqSelector((state) =>
+	const qStatus = useShallowEqSelector(selectQStatus);
+
+	const dispatch = useAppDispatch();
+	let question = useShallowEqSelector((state) =>
 		selectQuestion(state, Number(qId))
 	);
+
+	useEffect(() => {
+		if (!question) dispatch(handleShowQuestion(Number(qId)));
+	}, []);
+
 	return (
-		<Center mt="13vh">
+		<Center mt={["13vh", "16vh"]}>
 			<Stack
 				w={["full", "90vw"]}
 				maxW={"xl"}
 				spacing="2"
 				alignItems="flex-start"
 			>
-				<Button onClick={() => history.push("/")} w={[20, 32]} h={[7, 10]}>
+				<Button onClick={() => history.goBack()} w={[20, 32]} h={[7, 10]}>
 					Back
 				</Button>
-				<QuestionView question={question} currentUser={currentUser} />
+				{!question ? (
+					<Box padding="6" boxShadow="lg" bg="white" w="full">
+						<SkeletonCircle size="10" />
+						<SkeletonText mt="4" noOfLines={4} spacing="4" />
+					</Box>
+				) : (
+					<QuestionView question={question} currentUser={currentUser} qPage />
+				)}
 			</Stack>
 		</Center>
 	);
 };
 
 export default Question;
-
