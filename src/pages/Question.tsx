5d7f7fd90eaf71c27adaf6c5e17a1cbabda9eb56
Hossam Okasha <enghossamokashaa@gmail.com>
2021-11-12 01:05:10 +0200

    Handle failed show Q

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -9,18 +9,26 @@ import {
 import { useParams, useHistory } from "react-router-dom";
 import QuestionView from "../components/questionView";
 import { FC, useEffect } from "react";
-import { useShallowEqSelector, useAppDispatch } from "../utils/hooks";
+import {
+	useShallowEqSelector,
+	useAppDispatch,
+	useSearch,
+} from "../utils/hooks";
 import { selectProfile } from "../redux/slices/profileSlice";
 import {
 	handleShowQuestion,
+	selectQStatus,
 	selectQuestion,
 } from "../redux/slices/questionsSlice";
+import NoResult from "../components/noResult";
 
 interface QuestionViewProps {}
 const Question: FC<QuestionViewProps> = () => {
 	const history = useHistory();
 	const { qId } = useParams() as { qId: string | undefined };
+	const answerId = useSearch("answer_id").getValue();
 	const currentUser = useShallowEqSelector(selectProfile);
+	const qStatus = useShallowEqSelector(selectQStatus);
 
 	const dispatch = useAppDispatch();
 	let question = useShallowEqSelector((state) =>
@@ -29,6 +37,7 @@ const Question: FC<QuestionViewProps> = () => {
 
 	useEffect(() => {
 		if (!question) dispatch(handleShowQuestion(Number(qId)));
+		// if(qStatus === 'failed') history.
 	}, [question, qId]); // eslint-disable-line
 
 	return (
@@ -42,14 +51,26 @@ const Question: FC<QuestionViewProps> = () => {
 				<Button onClick={() => history.goBack()} w={[20, 32]} h={[7, 10]}>
 					Back
 				</Button>
-				{!question ? (
-					<Box padding="6" boxShadow="lg" bg="white" w="full">
-						<SkeletonCircle size="10" />
-						<SkeletonText mt="4" noOfLines={4} spacing="4" />
-					</Box>
-				) : (
-					<QuestionView question={question} currentUser={currentUser} qPage />
+				{qStatus === "failed" && (
+					<NoResult
+						heading="This Page Isn't Available Right Now"
+						text="This may be because of a technical error that we're working to get fixed. Try reloading this page."
+					/>
 				)}
+				{qStatus === "failed" ||
+					(!question ? (
+						<Box padding="6" boxShadow="lg" bg="white" w="full">
+							<SkeletonCircle size="10" />
+							<SkeletonText mt="4" noOfLines={4} spacing="4" />
+						</Box>
+					) : (
+						<QuestionView
+							answerId={answerId}
+							question={question}
+							currentUser={currentUser}
+							qPage
+						/>
+					))}
 			</Stack>
 		</Center>
 	);
