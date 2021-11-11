f209a4419e5d8a4c7b4d719b45f58bea2ca57f38
Hossam Okasha <enghossamokashaa@gmail.com>
2021-11-12 00:55:27 +0200

    Add noResult component

--- src/components/noResult.tsx
+++ src/components/noResult.tsx
@@ -1,16 +1,32 @@
 import { Text, Heading, VStack } from "@chakra-ui/layout";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import Icon from "@chakra-ui/icon";
+import { FC } from "react";
 
-const NoResult = () => {
+interface QuestionViewProps {
+	heading: string;
+	text: string;
+}
+const NoResult: FC<QuestionViewProps> = ({ heading, text }) => {
 	return (
-		<VStack pt="100px">
-			<Icon as={AiOutlineCloseCircle} h="80%" fontSize={["70px" ,"90px"]} color="blue.500"/>
-			<Heading as="h3" fontSize={["20px" ,"25px"]} mt="8" color="blue.500" pt="20px">
-				No results found
+		<VStack pt="100px" textAlign="center">
+			<Icon
+				as={AiOutlineCloseCircle}
+				h="80%"
+				fontSize={["70px", "90px"]}
+				color="gray.500"
+			/>
+			<Heading
+				as="h3"
+				fontSize={["20px", "25px"]}
+				mt="8"
+				color="gray.500"
+				pt="20px"
+			>
+				{heading}
 			</Heading>
-			<Text fontSize={["14px" ,"16px"]} color="gray.500">
-				Please check spelling or try different keywords.
+			<Text fontSize={["14px", "16px"]} color="gray.500">
+				{text}
 			</Text>
 		</VStack>
 	);
